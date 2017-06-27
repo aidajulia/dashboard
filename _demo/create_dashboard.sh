c4467a7dff724feb5617962b1ca49f7da38991a6
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-06-27 18:16:49 +0200

    Demo fixes
    * update tiles' ids in 2x4 template to numeric
    * feed.py sends data 2 ways:
        * an old way to work with dashboard-toolkit
        * a new way to work with Dashboard demo
    * add script creating demo dashboard

--- _demo/create_dashboard.sh
+++ _demo/create_dashboard.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-curl 'http://localhost:8000/gui-api/dashboard' \
+curl 'http://0.0.0.0:8000/gui-api/dashboard' \
     -H 'content-type: application/json' \
     --data-binary '{"name":"demo","owner_email":"email@subdomain.com","layout":"2x4"}'
-
